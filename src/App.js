35241d966686586245f2a3fa38f1978e4dfac43c
Russian-Idiot <ranil2086@bk.ru>
2023-01-24 15:33:51 +0500

    little changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React from "react";
+
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import Card from "./components/Card";
+
+import "./scss/app.scss";
 
 function App() {
   return (
-    <></>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">All pizzas</h2>
+          <div className="content__items">
+            {[...Array(12)].map((item, index) => {
+              return <Card key={index} title={'pizza'} price={'400'} imageUrl={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"} />
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">All</li>
+        <li>Meat</li>
+        <li>Vegetarian</li>
+        <li>Grill</li>
+        <li>Spicy</li>
+        <li>Closed</li>
+      </ul>
+    </div>
+  );
+}
