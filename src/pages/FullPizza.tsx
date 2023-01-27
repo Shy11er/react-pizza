6a29eff20c9fe9d2b56e591b4370d552b3e178a6
Russian-Idiot <ranil2086@bk.ru>
2023-01-28 00:06:26 +0500

    TypeScript

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -3,16 +3,20 @@ import { useParams, useNavigate } from "react-router-dom";
 
 import axios from "axios";
 
-const FullPizza = () => {
+const FullPizza: React.FC = () => {
   const { id } = useParams();
-  const [items, setItems] = React.useState();
+  const [items, setItems] = React.useState<{
+    imageUrl: string;
+    title: string;
+    price: number; 
+  }>();
   const navigate = useNavigate();
 
   React.useEffect(() => {
     async function fetchData() {
       try {
         const { data } = await axios.get(
-          `https://63c418a0a908563575316ae6.mockapi.io/items/${id.toString()}`
+          `https://63c418a0a908563575316ae6.mockapi.io/items/${id}`
         );
 
         setItems(data);
@@ -26,10 +30,10 @@ const FullPizza = () => {
     fetchData();
   }, []);
 
-  if (!items) return;
+  if (!items) return <>Loading...</>;
 
   return (
-    <div>
+    <div className="fullItem">
       <img src={items.imageUrl} alt="pizza" />
       <h2>{items.title}</h2>
       <h4>{items.price} $</h4>
