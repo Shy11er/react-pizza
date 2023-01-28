02dd66645145a940832a7dce183f1d0a227e67c0
Russian-Idiot <ranil2086@bk.ru>
2023-01-28 12:38:05 +0500

    update

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -3,22 +3,20 @@ import ReactPaginate from "react-paginate";
 
 type PaginationProps = {
   onClickCurrentPage: (num: number) => void;
-}
-
-const Pagination: React.FC<PaginationProps> = ({onClickCurrentPage}) => {
-  return (
-    <ReactPaginate
-      className="pagination"
-      breakLabel="..."
-      previousLabel="<"
-      nextLabel=">"
-      onPageChange={(e) => {
-        onClickCurrentPage(e.selected + 1);
-      }}
-      pageRangeDisplayed={8}
-      pageCount={3}
-    />
-  );
 };
+
+const Pagination: React.FC<PaginationProps> = ({ onClickCurrentPage }) => (
+  <ReactPaginate
+    className="pagination"
+    breakLabel="..."
+    previousLabel="<"
+    nextLabel=">"
+    onPageChange={(e) => {
+      onClickCurrentPage(e.selected + 1);
+    }}
+    pageRangeDisplayed={8}
+    pageCount={3}
+  />
+);
 
 export default Pagination;
